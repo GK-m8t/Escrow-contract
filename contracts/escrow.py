226277ce4366f4261355209f2db7de031fd3c2fb
GK-m8t <52692944+GK-m8t@users.noreply.github.com>
2020-06-14 13:58:07 +0530

    Update escrow.py

--- contracts/escrow.py
+++ contracts/escrow.py
@@ -1,43 +1,38 @@
 import smartpy as sp
-
-#Both buyer and seller must send in twice the value of the item they are transacting on. This ensures that both parties are committed to see the transaction through to the end. 
-
+#buyer must send in twice the value of the item they are transacting on,while the seller sends in the value of the item. This ensures that both parties are committed to see the transaction through to the end.
 class Escrow(sp.Contract):
     def __init__(self):
         self.init(seller = sp.none, buyer = sp.none, price = 0)
-
     @sp.entry_point
     def setSeller(self, params):
         #ensure seller hasn't already been set
-        sp.verify (~self.data.seller.isSome())
+        sp.verify (~self.data.seller.is_some())
         
-        #the seller sets the price and must send 2x the price in tez
+        #the seller sets the price and must send the price in tez as insurance
         self.data.price = params.price
-        sp.verify (sp.amount == sp.tez(self.data.price * 2))
-        self.data.seller = sp.Some(sp.sender)
-
+        sp.verify (sp.amount == sp.tez(self.data.price))
+        self.data.seller = sp.some(sp.sender)
     @sp.entry_point
-    def setBuyer(self, params):
+    def setBuyer(self):
         #ensure that there already is a seller
-        sp.verify (self.data.seller.isSome())
+        sp.verify (self.data.seller.is_some())
         #ensure buyer hasnt already been set
-        sp.verify (~self.data.buyer.isSome())
+        sp.verify (~self.data.buyer.is_some())
         
         sp.verify (sp.amount == sp.tez(self.data.price * 2))
-        self.data.buyer = sp.Some(sp.sender)
+        self.data.buyer = sp.some(sp.sender)
         
     @sp.entry_point
-    def confirmReceived(self, params):
-        sp.verify (sp.sender == self.data.buyer.openSome())
-        sp.send (self.data.buyer.openSome(), sp.tez(self.data.price))
-        sp.send (self.data.seller.openSome(), sp.balance)
+    def confirmReceived(self):
+        sp.verify (sp.sender == self.data.buyer.open_some())
+        sp.send (self.data.buyer.open_some(), sp.tez(self.data.price))
+        sp.send (self.data.seller.open_some(), sp.balance)
         self.resetContract()
-
     @sp.entry_point
-    def refundBuyer(self, params):
-        sp.verify (sp.sender == self.data.seller.openSome())
-        sp.send (self.data.buyer.openSome(), sp.tez(2 * self.data.price))
-        sp.send (self.data.seller.openSome(), sp.balance)
+    def refundBuyer(self):
+        sp.verify (sp.sender == self.data.seller.open_some())
+        sp.send (self.data.buyer.open_some(), sp.tez(2 * self.data.price))
+        sp.send (self.data.seller.open_some(), sp.balance)
         self.resetContract()
         
     #clear out buyer and seller
@@ -45,25 +40,25 @@ class Escrow(sp.Contract):
         self.data.buyer = sp.none
         self.data.seller = sp.none
         self.data.price = 0
-
+        
 @sp.add_test(name = "Test Escrow")
 def testEscrow():
-  html = ""
-  seller = "AAA"
-  buyer = "BBB"
-
-  myContract = Escrow()
-
-  #set the seller and price
-  html += myContract.setSeller(price = 1).run(sp.address(seller), amount = sp.tez(2)).html()
+    html = sp.test_scenario()
+    
+    html.h1("Escrow Cont")
+    seller = sp.test_account("seller")
+    buyer = sp.test_account("buyer")
+    
+    myContract = Escrow()
+     #set the seller and price
+    html += myContract
+    
+    html += myContract.setSeller(price = 1).run(sender = seller, amount = sp.tez(1))
+     #set the buyer
+    html += myContract.setBuyer().run(sender = buyer, amount = sp.tez(2))
+    # buyer confirms they received item 
+    html += myContract.confirmReceived().run(sender = buyer)
   
-  #set the buyer
-  html += myContract.setBuyer().run(sp.address(buyer), amount = sp.tez(2)).html()
+     # seller decides to refund buyer
+     # html += myContract.refundBuyer().run(sp.address(seller)).html()
   
-  # buyer confirms they received item 
-  html += myContract.confirmReceived().run(sp.address("BBB")).html()
-  
-  # seller decides to refund buyer
-  # html += myContract.refundBuyer().run(sp.address(seller)).html()
-  
-  setOutput(html)
